76e9fdbeb4aec3eba516c0e442a0972f67062083
ztretterath <ztretterath@gmail.com>
2016-07-13 16:10:41 -0400

    added memory game logic
    aded css and js and edited html to call it

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,62 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-if (cardTwo == cardFour) {
-  alert ("you found a match!")
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    
+    cardElement.className = 'card';
+		
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
 }
+
+function isTwoCards()
+{
+  // bug: need to check if the clicked card is already flipped
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king')
+  {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+	}
+  else
+  {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+	}
+
+  if (cardsInPlay.length === 2)
+  {
+    if (!isMatch(cardsInPlay))
+    {
+      alert("images are yellow!!!");
+      // todo: fix the images to yellow
+    }
+    cardsInPlay = [];
+  }
+}
+
+function isMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1])
+  {
+    alert ("You found a match!");
+    return true;
+  }
+  else
+  {
+	  alert ("Try again!");
+    return false;
+	}
+}
+
+createBoard();
